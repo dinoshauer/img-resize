79a267801ab766c95ad1b5dc1da9b4c963223e84
Kasper Jacobsen <k@mackwerk.dk>
2014-04-23 18:32:21 +0200

    Started class in logic

--- resizer/logic.py
+++ resizer/logic.py
@@ -1,4 +1,10 @@
 import os
+import redis
+import resize
+
+class Resizer:
+    def __init__(self, redis_config):
+        self.r = redis.Redis(**redis_config)
 
 def read_to_bytes(src):
     with open(src, 'rb') as f:
