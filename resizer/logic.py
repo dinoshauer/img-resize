4f6099d5741cc171a36e12b81150f55818f52769
Kasper Jacobsen <k@mackwerk.dk>
2014-04-23 18:42:17 +0200

    Moved methods to class and imported requests

--- resizer/logic.py
+++ resizer/logic.py
@@ -1,18 +1,24 @@
 import os
+
 import redis
+import requests
+
 import resize
+
 
 class Resizer:
     def __init__(self, redis_config):
         self.r = redis.Redis(**redis_config)
 
-def read_to_bytes(src):
-    with open(src, 'rb') as f:
-        return f.read()
+    def
 
-def remove_file(src):
-    try:
-        os.remove(src)
-        return True
-    except IOError:
-        return False
+    def read_to_bytes(self, src):
+        with open(src, 'rb') as f:
+            return f.read()
+
+    def remove_file(self, src):
+        try:
+            os.remove(src)
+            return True
+        except IOError:
+            return False
