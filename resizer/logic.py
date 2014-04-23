fa0ade45e95aac2775b4fd29835dc3f26450d710
Kasper Jacobsen <k@mackwerk.dk>
2014-04-23 19:43:50 +0200

    added #process, #get_image, #resize and importing uuid for thumbnail filename

--- resizer/logic.py
+++ resizer/logic.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 
 import redis
 import requests
@@ -7,10 +8,27 @@ import resize
 
 
 class Resizer:
-    def __init__(self, redis_config):
+    def __init__(self, redis_config, image_dir):
         self.r = redis.Redis(**redis_config)
+        self.get = requests.get
+        self.image_dir = image_dir
 
-    def
+    def process(self, kwargs):
+        print kwargs
+        return {'status': 'ok'}, 200
+
+    def get_image(self, src):
+        request = self.get(src)
+        if request.ok:
+            for c in request.iter_content:
+                print c
+                print '*****~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*****'
+
+    def resize_image(self, src, w, h):
+        out = '{}/{}.jpg'.format(self.image_dir, str(uuid.uuid4()))
+        result = resize.resize(src, out, w, h)
+        if result:
+            return out
 
     def read_to_bytes(self, src):
         with open(src, 'rb') as f:
