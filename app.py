b1f819860f17355f8b4f4320f7da645de1b1b97d
Kasper Jacobsen <k@mackwerk.dk>
2014-04-23 19:42:00 +0200

    added blueprint

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import sys
 from flask import Flask
 
 from config import load_config
-
+from resizer.routes import resizer
 
 app = Flask(__name__)
 load_config(app, sys.argv)
+app.register_blueprint(resizer)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
