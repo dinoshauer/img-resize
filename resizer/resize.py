3f8115fbca4bc8da02dc4616ce299c4986bc5eb5
Kasper Jacobsen <k@mackwerk.dk>
2014-04-23 18:15:49 +0200

    Resizing works with jpg, png, gif, bmp

--- resizer/resize.py
+++ resizer/resize.py
@@ -1,25 +1,29 @@
 from PIL import Image
 
-def resize(src, out, w, h):
+def resize(src, out, w, h, quality=70):
     try:
         image = Image.open(src)
-        print 'image', image
-        print 'image.thumbnail((w, h), Image.ANTIALIAS)', image.thumbnail((w, h), Image.ANTIALIAS)
-        print 'image.save(out, "JPEG")', image.save(out, "JPEG")
+        image.thumbnail((w, h), Image.ANTIALIAS)
+        image.convert('RGB').save(out, "JPEG", quality=quality)
     except IOError, e:
         print 'IOError'
         print e
 
-
-if __name__ == '__main__':
+def main():
     import sys
 
     try:
         src = sys.argv[1]
         out = sys.argv[2]
-        w = sys.argv[3]
-        h = sys.argv[4]
+        w = int(sys.argv[3])
+        h = int(sys.argv[4])
         resize(src, out, w, h)
+        sys.exit(0)
     except IndexError:
         print 'Missing argument, please use the resizer like this:'
         print 'python {} source_image output_image width height'.format(__file__)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
