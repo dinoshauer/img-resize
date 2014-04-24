88439ad8cb57d2034486b370c1406310e70425e9
Kasper Jacobsen <k@mackwerk.dk>
2014-04-24 14:55:07 +0200

    Removed unnecessary view functions

--- resizer/routes.py
+++ resizer/routes.py
@@ -11,24 +11,6 @@ resizer = Blueprint(
 )
 
 @resizer.route('/', methods=['GET'])
-def get():
-    try:
-        r = Resizer(
-            current_app.config['REDIS'],
-            current_app.config['IMAGE_DIR'],
-            current_app.config['REDIS_KEY_EXPIRE']
-        )
-        result = r.process(request.args)
-        if result:
-            return Response(
-                result,
-                mimetype='image/jpeg'
-            )
-        return responses.not_found()
-    except KeyError, e:
-        return responses.bad_argument(e.message)
-
-@resizer.route('/direct', methods=['GET'])
 def pass_through():
     try:
         r = Resizer(
@@ -45,14 +27,3 @@ def pass_through():
         return responses.not_found()
     except KeyError, e:
         return responses.bad_argument(e.message)
-
-@resizer.route('/img/<file_name>')
-def get_file(file_name):
-    r = ImageRetriever(current_app.config['REDIS'])
-    result = r.get_file(file_name)
-    if result:
-        return Response(
-            result,
-            mimetype='image/jpeg'
-        )
-    return responses.not_found()
