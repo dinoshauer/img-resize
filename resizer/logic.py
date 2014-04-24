743c12be3f80476ff7ada73ef6bb67b5ae02a2a0
Kasper Jacobsen <k@mackwerk.dk>
2014-04-24 14:42:27 +0200

    Added url_parse method, added MAGIC for image file type detectiveness

--- resizer/logic.py
+++ resizer/logic.py
@@ -1,9 +1,11 @@
 import os
 import uuid
 import io
+from urlparse import urlparse
 
 import redis
 import requests
+import magic
 
 import resize
 
@@ -16,6 +18,7 @@ class ImageRetriever:
 
 class Resizer:
     def __init__(self, redis_config, image_dir, key_expire=None):
+        self.redis_config = redis_config
         self.r = redis.Redis(**redis_config)
         self.get = requests.get
         self.key_expire = key_expire
@@ -40,16 +43,20 @@ class Resizer:
         if file_exists:
             return file_exists
         result = self.process(kwargs, file_name=file_name)
-        return image.get_file(result['file_name'])
+        if result:
+            return image.get_file(result['file_name'])
+        return False
 
     def process(self, kwargs, file_name=None):
         src = self.download_image(kwargs['src'])
-        w = int(kwargs['w'])
-        h = int(kwargs['h'])
-        thumb = self.resize_image(src, w, h, file_name)
-        base_name = os.path.basename(thumb)
-        if self.to_redis(base_name, thumb):
-            return {'filename': base_name}
+        if self._check_file_type(src):
+            w = int(kwargs['w'])
+            h = int(kwargs['h'])
+            thumb = self.resize_image(src, w, h, file_name)
+            base_name = os.path.basename(thumb)
+            if self.to_redis(base_name, thumb):
+                return {'file_name': base_name}
+        return False
 
     def to_redis(self, name, thumb):
         if self.r.setex(name, self.read_to_bytes(thumb), self.key_expire):
@@ -63,7 +70,7 @@ class Resizer:
 
     def resize_image(self, src, w, h, file_name=None):
         if file_name:
-            out = '{}/{}.jpg'.format(self.image_dir, file_name)
+            out = '{}/{}'.format(self.image_dir, file_name)
         else:
             out = '{}/{}.jpg'.format(self.image_dir, str(uuid.uuid4()))
         result = resize.resize(src, out, w, h)
