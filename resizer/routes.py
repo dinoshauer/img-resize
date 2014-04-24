e8f1ad5ce98cf2bcb5ef77eb04000cbd6ccc52e5
Kasper Jacobsen <kasper@falconsocial.com>
2014-04-24 13:06:37 +0200

    Added direct route

--- resizer/routes.py
+++ resizer/routes.py
@@ -23,6 +23,22 @@ def get():
     except KeyError, e:
         return responses.bad_argument(e.message)
 
+@resizer.route('/direct', methods=['GET'])
+def pass_through():
+    try:
+        r = Resizer(
+            current_app.config['REDIS'],
+            current_app.config['IMAGE_DIR'],
+            current_app.config['REDIS_KEY_EXPIRE']
+        )
+        result = r.process_direct(request.args)
+        return Response(
+            result,
+            mimetype='image/jpeg'
+        )
+    except KeyError, e:
+        return responses.bad_argument(e.message)
+
 @resizer.route('/img/<file_name>')
 def get_file(file_name):
     r = ImageRetriever(current_app.config['REDIS'])
