73fc4af6a7393296f0428420fbc321e2a8195614
Kasper Jacobsen <k@mackwerk.dk>
2014-05-05 15:30:23 +0200

    Updated deploy

--- _tools/deploy.py
+++ _tools/deploy.py
@@ -2,9 +2,8 @@ import glob
 from fabric.api import cd, local, run, put, sudo, prefix
 from fabric.contrib.project import rsync_project
 
-WEB_ROOT = '/var/www/img-resizer/'
-UWSGI_CONFIG_DIR = '/etc/uwsgi/apps-available/'
-NGINX_CONF_DIR = '/etc/nginx/sites-available/'
+WEB_ROOT = '/home/ubuntu/img-resizer/'
+NGINX_CONF_DIR = '/etc/nginx/sites-enabled/'
 
 def deploy_api():
     rsync_project(
@@ -13,20 +12,19 @@ def deploy_api():
     )
 
 def build_virtual_env():
-    with cd(WEB_ROOT), prefix('. env/bin/activate'):
-        run('pip install -q -r requirements.txt')
+    with cd(WEB_ROOT), prefix('. /home/ubuntu/.virtualenvs/img-resizer/bin/activate'):
+        run('pip install -I -r requirements.txt')
+
+def re_link_nginx_config():
+    with cd(NGINX_CONF_DIR):
+        sudo('ln -s --force {}_conf/img-resizer.nginx'.format(WEB_ROOT))
 
 def reload_nginx_config():
     sudo('service nginx reload')
 
-def deploy_uwsgi_config():
-    put(local_path='_conf/img-resizer.uwsgi.ini',
-        remote_path=UWSGI_CONFIG_DIR,
-        use_sudo=True)
-
-def update_uwsgi_symlink():
-    with cd('/etc/uwsgi/apps-enabled'):
-        sudo('ln -s --force ../apps-available/img-resizer.uwsgi.ini img-resizer.uwsgi.ini')
+def restart_supervisor_processes():
+    for i in xrange(4):
+        sudo('supervisorctl restart img-resizer:img-resizer-{}'.format(i))
 
 def restart_uwsgi():
     sudo('touch /etc/uwsgi/apps-enabled/img-resizer.uwsgi.ini')
@@ -34,7 +32,6 @@ def restart_uwsgi():
 def build_and_deploy():
     deploy_api()
     build_virtual_env()
+    re_link_nginx_config()
     reload_nginx_config()
-    deploy_uwsgi_config()
-    update_uwsgi_symlink()
-    restart_uwsgi()
+    restart_supervisor_processes()
